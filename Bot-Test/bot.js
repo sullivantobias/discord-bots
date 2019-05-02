2e8a35e522e3e3588b0732e0c944a4a259f93a11
Sullivan Tobias <githubst@gmail.com>
2019-05-02 16:29:58 +0200

    Update

--- Bot-Test/bot.js
+++ Bot-Test/bot.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.on('ready',  () => {
-  console.log("Je suis connecté !")
+bot.on('ready', () => {
+  console.log("On...")
 });
 
-bot.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong !')
-  }
+bot.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'hello');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
 });
 
-bot.login('NTczNDE2MjA1NTMzMTg0MDAw.XMr3OA.eGKl5HclYm2VPPjIeL3qTzaWeAY');
+bot.login(process.env.BOT_TOKEN);

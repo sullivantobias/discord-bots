0762ae9be42be52cbfb3b9285d0ee2ab57378de0
Sullivan Tobias <githubst@gmail.com>
2019-05-03 11:20:17 +0200

    Feature Give a default role

--- Bot-Test/scripts/GuildMemberManagement.js
+++ Bot-Test/scripts/GuildMemberManagement.js
@@ -6,19 +6,29 @@ class GuildMemberManagement {
     this.bot = bot
   }
 
+  writeManagement(member, message) {
+    const channel = member.guild.channels.find(ch => ch.name === 'hello');
+    if (!channel) return;
+    channel.send(message)
+  }
+
   guildMemberIn() {
     this.bot.on('guildMemberAdd', member => {
-      const channel = member.guild.channels.find(ch => ch.name === 'hello');
-      if (!channel) return;
-      channel.send(`${member}, Welcome to our server ✅`)
+      // give the default role for new user //
+      const role = member.guild.roles.find('name', 'Craqueboule');
+      member.addRole(role)
+        .then(console.log)
+        .catch(console.error);
+
+      // say hello to new user //
+      this.writeManagement(member, `${member}, Welcome to our server ✅`)
     });
   }
 
   guildMemberOut() {
     this.bot.on('guildMemberRemove', member => {
-      const channel = member.guild.channels.find(ch => ch.name === 'hello');
-      if (!channel) return;
-      channel.send(`${member} decided to leave ❌`)
+      // say farewell to new user //
+      this.writeManagement(member, `${member} decided to leave ❌`)
     });
   }
 }

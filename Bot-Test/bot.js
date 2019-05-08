ede7e1a5530302fbebc1ab1b18ec27b4722fc797
Sullivan Tobias <githubst@gmail.com>
2019-05-09 00:32:30 +0200

    Test Fix

--- Bot-Test/bot.js
+++ Bot-Test/bot.js
@@ -2,14 +2,19 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
-  console.log("On...")
+  console.log("Runnin...")
 });
 
-const {GuildMemberManagement} = require('./scripts/GuildMemberManagement');
+const { GuildMemberManagement } = require('./scripts/GuildMemberManagement');
 const GuildManagement = new GuildMemberManagement(bot);
 // start users management //
 GuildManagement.guildMemberIn();
 GuildManagement.guildMemberOut();
 // end users management //
+
+const { ManageMessages } = require('./scripts/ManageMessages');
+const ManageMsg = new ManageMessages(bot);
+
+ManageMsg.manageMessages();
 
 bot.login(process.env.BOT_TOKEN);

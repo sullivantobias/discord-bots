7426218b2652550007b802bac1676d15fac84ca8
Tobias <stobias@sqli.com>
2019-05-09 16:23:51 +0200

    Fix words filter

--- Bot-Test/bot.js
+++ Bot-Test/bot.js
@@ -1,18 +1,18 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-bot.on('ready', () => {
-  console.log("Runnin...")
+bot.on("ready", () => {
+  console.log("Runnin...");
 });
 
-const { GuildMemberManagement } = require('./scripts/GuildMemberManagement');
+const { GuildMemberManagement } = require("./scripts/GuildMemberManagement");
 const GuildManagement = new GuildMemberManagement(bot);
 // start users management //
 GuildManagement.guildMemberIn();
 GuildManagement.guildMemberOut();
 // end users management //
 
-const { ManageMessages } = require('./scripts/ManageMessages');
+const { ManageMessages } = require("./scripts/ManageMessages");
 const ManageMsg = new ManageMessages(bot);
 
 ManageMsg.manageMessages();
--- Bot-Test/scripts/ManageMessages.js
+++ Bot-Test/scripts/ManageMessages.js
@@ -1,43 +1,56 @@
 /**
  * @class ManageMessages
  */
-const frenchWords = require('../data/blacklistfrenchwords')
-const englishWords = require('../data/blacklistenglishwords')
+const frenchWords = require("../data/blacklistfrenchwords");
+const englishWords = require("../data/blacklistenglishwords");
 
 class ManageMessages {
-    constructor(bot) {
-        this.bot = bot;
-    }
+  constructor(bot) {
+    this.bot = bot;
+  }
 
-    manageMessages() {
-        this.bot.on('message', message => {
-            const blacklistFR = frenchWords;
-            const blacklistEN = englishWords;
+  manageMessages() {
+    this.bot.on("message", message => {
+      const blacklistFR = frenchWords;
+      const blacklistEN = englishWords;
 
-            this.warningAndLog(message, blacklistFR, "FR")
-            this.warningAndLog(message, blacklistEN, "EN")
-        })
-    }
-    warningAndLog(message, list, lang) {
-        // check if it's not a bot //
-        if (!message.author.bot) {
-            list.blacklist.forEach(word => {
-                if (message.toString().toLowerCase().includes(word)) {
-                    // get log channel //
-                    const log = this.bot.channels.find(channel => channel.name === "logs")
-                    // warning message
-                    let warning = `${message.author} | '${word}' n'est pas autorisé, Attention au language 👿`
-                    // check lang (fr by default) //
-                    if (lang === 'EN')
-                        warning = `${message.author} | '${word}' is forbidden , pay attention to your language 👿`
+      this.warningAndLog(message, blacklistFR, "FR");
+      this.warningAndLog(message, blacklistEN, "EN");
+    });
+  }
+  warningAndLog(message, list, lang) {
+    // check if it's not a bot //
+    if (!message.author.bot) {
+      list.blacklist.forEach(word => {
+        if (
+          message
+            .toString()
+            .toLowerCase()
+            .trim()
+            .split(" ")
+            .includes(word)
+        ) {
+          // get log channel //
+          const log = this.bot.channels.find(
+            channel => channel.name === "logs"
+          );
+          // warning message
+          let warning = `${
+            message.author
+          } | '${word}' n'est pas autorisé, Attention au language 👿`;
+          // check lang (fr by default) //
+          if (lang === "EN")
+            warning = `${
+              message.author
+            } | '${word}' is forbidden , pay attention to your language 👿`;
 
-                    message.channel.send(warning);
-                    // logs //
-                    log.send(`${message.author} used the word: ${word}`);
-                }
-            });
+          message.channel.send(warning);
+          // logs //
+          log.send(`${message.author} used the word: ${word}`);
         }
+      });
     }
+  }
 }
 
 module.exports.ManageMessages = ManageMessages;

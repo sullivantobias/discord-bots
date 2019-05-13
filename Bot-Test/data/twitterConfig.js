e76665da42d68697c96dbb3126f8ae48a9f57962
Sullivan Tobias <githubst@gmail.com>
2019-05-13 17:45:31 +0200

    Refacto

--- Bot-Test/data/twitterConfig.js
+++ Bot-Test/data/twitterConfig.js
@@ -1,8 +1,8 @@
 const config = {
-    consumer_key: 'toRy0glo5cRxIFkZKHkNBik0e',
-    consumer_secret: 'QBKoOYw4hxqaA4kVxd7FDv2mTuglZ7IrO8cS9F6ikQjcM3ygQh',
-    access_token_key: '1072506751549886464-dH4xVkRI8wXyGqdJmL6xKzyy0uktMl',
-    access_token_secret: '2uWbUY0ZTHmSXVlthNhA26kVtt4aTLOMkt9MyI72wLAmP'
+    consumer_key: process.env.TWITTER_KEY,
+    consumer_secret: process.env.TWITTER_SECRET,
+    access_token_key: process.env.TOKEN_KEY,
+    access_token_secret: process.env.TOKEN_SECRET
 }
 
 module.exports.Config = config
